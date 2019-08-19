80aed2c58a2e99ffa1374b7765e0287159d6a8ff
danieloh <daniel.oe.halvorsen@gmail.com>
2019-08-19 15:53:37 +0200

    Added exact T loop

--- TEP4165_exercise_1_python.py
+++ TEP4165_exercise_1_python.py
@@ -4,4 +4,20 @@
 
 import numpy as np
 
-x = 5
+L = 1
+time = np.linspace(0, 25, 5)
+start = 0
+end = 0.01
+points = 100
+x = np.linspace(start, end, points)
+alpha = 1
+T_l = 200
+T_r = 600
+iterations = 50
+T = np.zeros([len(t),len(x)])
+T[:,:] = ((T_l + T_r) / 2)
+for t_idx, t in enumerate(time):
+    for i in range(iterations):
+        T[:] = T + ((2 * (T_l + T_r)) / (np.pi * i)) * np.sin(i * np.pi / 2) * np.cos(
+            np.pi * i * x / L) * np.exp((-alpha * (np.pi * i / L) ** 2) * t)
+
