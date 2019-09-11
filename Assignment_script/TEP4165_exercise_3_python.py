2a850236becf007b2a1106ce6cbdb61060244dde
danielhalvorsen <daniel.oe.halvorsen@gmail.com>
2019-09-11 23:17:40 +0200

    commented script

--- Assignment_script/TEP4165_exercise_3_python.py
+++ Assignment_script/TEP4165_exercise_3_python.py
@@ -10,17 +10,21 @@ from matplotlib import rc
 
 
 def upwind(u, Tb, NJ, T_0, C, t_end):
+    # Explicit upwind FVM algorithm
+    # Initializing constants and variables
     dx = 1 / NJ
     dt = C * dx / u
     timesteps = int(np.ceil(t_end / dt))
     T = T_0.copy()
     T_new = T.copy()
-    print('Length of T: '+str(len(T)))
+
     for n in range(timesteps):
-       # for j in range(NJ - 1):
-       #     T_new[j] = (1 + C) * T[j] - C * T[j + 1]
+    # Temporal loop
+        #Spatial loop using array operations
         T_new[1:NJ-1] = (1 + C) * T[1:NJ-1] - C * T[2:NJ]
         T_new[-1] = Tb
+
+        # Update old variable with new data
         T = T_new.copy()
     return T
 
@@ -31,6 +35,7 @@ rc('font', **{'family': 'sans-serif', 'sans-serif': ['Helvetica']})
 rc('text', usetex=True)
 
 if __name__ == '__main__':
+    # Declaration of variables and constants
     Tb = 200
     NJ = 100
     C = -0.5
@@ -38,6 +43,8 @@ if __name__ == '__main__':
     t_end_list = [0.04,0.5,1.2]
     L = 1
     x = np.linspace(0, L, NJ)/L
+
+    # Initial vector
     T_left = 800
     T_right = 200
     T_0_left = np.ones(int(NJ / 2)) * T_left
@@ -48,18 +55,10 @@ if __name__ == '__main__':
     ax = fig1.add_subplot(111)
 
     for t_end in t_end_list:
-        print(t_end)
-
+    # Loop over different time levels
         T = upwind(u, Tb, NJ, T_0, C, t_end)
         ax.plot(x,T,marker='d',markersize=3,linestyle='-',linewidth=0.3,label='Upwind, t= '+np.str(t_end))
         ax.legend(loc=0)
-
-        #ax2.plot(x_vec / x_vec[-1], T_analytical[0][t_idx, :], linestyle='-',
-         #        linewidth=1, label='Exact t= ' + np.str(t))
-        #ax2.legend(loc=4)
-
-
-
     plt.xlabel('$x/L (-)$')
     plt.ylabel('Temperature, $T (K)$')
     plt.savefig('expl_upwind_C-05.png')
